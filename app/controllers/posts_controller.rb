24380e5fd7b9f239e2b12da6512f6147bd74e581
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-04-03 15:13:54 -0500

    lkej

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -48,18 +48,18 @@ class PostsController < ApplicationController
         format.html { render :edit }
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # DELETE /posts/1
-  # DELETE /posts/1.json
-  def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  #   end
+  # end
+  #
+  # # DELETE /posts/1
+  # # DELETE /posts/1.json
+  # def destroy
+  #   @post.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to posts_url, notice: 'Post was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
