ae016c1d9a3fdba5edab5c94bb082614c57b7c46
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-04-03 15:09:40 -0500

    kjshdfk

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,19 +23,19 @@ class PostsController < ApplicationController
 
   # POST /posts
   # POST /posts.json
-  def create
-    @post = Post.new(post_params)
-
-    respond_to do |format|
-      # if @post.save
-      #   format.html { redirect_to @post, notice: 'Post was successfully created.' }
-      #   format.json { render :show, status: :created, location: @post }
-      # else
-      #   format.html { render :new }
-      #   format.json { render json: @post.errors, status: :unprocessable_entity }
-      # end
-    end
-  end
+  # def create
+  #   @post = Post.new(post_params)
+  #
+  #   respond_to do |format|
+  #     # if @post.save
+  #     #   format.html { redirect_to @post, notice: 'Post was successfully created.' }
+  #     #   format.json { render :show, status: :created, location: @post }
+  #     # else
+  #     #   format.html { render :new }
+  #     #   format.json { render json: @post.errors, status: :unprocessable_entity }
+  #     # end
+  #   end
+  # end
 
   # PATCH/PUT /posts/1
   # PATCH/PUT /posts/1.json
